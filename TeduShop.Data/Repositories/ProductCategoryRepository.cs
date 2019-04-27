039fd71ac9dec4261170cd03b0b90d5b069fa6af
thuongtin1992 <thuongtin1992@gmail.com>
2019-04-27 19:55:32 +0700

    bai 7

---
+++ TeduShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IProductCategoryRepository
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+        public ProductCategoryRepository(IDbFactory dbFactory) : base(dbFactory){ }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return DbContext.ProductCategories.Where(m => m.Alias == alias);
+        }
+    }
+}
