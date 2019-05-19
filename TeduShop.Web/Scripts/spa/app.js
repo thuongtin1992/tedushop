9d96a5c0914cf218a3f5c83cc281f5aad0402f66
thuongtin1992 <thuongtin1992@gmail.com>
2019-05-19 11:48:25 +0700

    bai 21 angularjs

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module('myModule', []);
+
+//myApp.controller("myController", function ($scope) {
+//    $scope.message = "This is my message from Controller!";
+//}); viết gộp
+
+// viết tường minh
+myApp.controller("schoolController", schoolController);
+myApp.directive("teduShopDirective", teduShopDirective);
+
+myApp.service('validatorService', validatorService);
+schoolController.$inject = ['$scope', 'validatorService'];
+
+//declare
+//rootScope
+//function studentController($rootScope, $scope) {
+//    $rootScope.message = "This is a student name A!";
+//}
+
+function schoolController($scope, validatorService) {
+    //Validator.checkNumber(1);
+    $scope.checkNumber = function () {
+        $scope.message = validatorService.checkNumber($scope.numb);
+    }
+    $scope.numb = 1;
+}
+
+function validatorService($window) {
+    return {
+        checkNumber : checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            //$window.alert('This is even.');
+            return 'This is even';
+        }
+        else
+            //$window.alert('This is odd.');
+            return 'This is odd';
+    }
+}
+
+function teduShopDirective() {
+    return {
+        restrict: "A",
+        templateUrl: "/Scripts/spa/teduShopDirective.html"
+    }
+}
